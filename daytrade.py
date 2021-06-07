ffda3ac3f8d1b079d0d50252a88e878c7908a933
Mahela-DESKTOP <mahela37@gmail.com>
2021-06-06 17:15:18 -0700

    break down yields per day

--- daytrade.py
+++ daytrade.py
@@ -1,8 +1,11 @@
 import json
 import os
 from json2html import *
+import datetime 
 
 MIN_PRINT_LEVEL=1
+MARKET_OPEN_SECONDS=60*60*6+60*30   # 6:30 AM in seconds
+SAMPLING_RATE_SECONDS=60
 
 # reduce clutter by defining print levels
 def custom_print(data,level):
@@ -16,15 +19,17 @@ class Simulation():
     transaction_fees=0
     prices=[]
     params=None
+    file=None
 
     def __init__(self,file,params):
-        self.load_prices(file)
+        self.file=file
+        self.load_prices()
         self.params=params
         self.balance=self.params['starting_value']
 
-    def load_prices(self,file):
+    def load_prices(self):
         # read data from file
-        with open (f"data_include/{file}","r") as f:
+        with open (f"data_include/{self.file}","r") as f:
             intraday=json.load(f)
         prices_raw=[]
         for entry in intraday:
@@ -56,17 +61,30 @@ class Simulation():
         self.has_stock=False
         custom_print(f"sold {self.params['num_shares']} shares at {price}. Balance is {self.balance}",0)
 
+    def get_timestamp(self,index):
+        market_open=datetime.timedelta(seconds=MARKET_OPEN_SECONDS)
+        delta_index=datetime.timedelta(seconds=index*SAMPLING_RATE_SECONDS)
+        current_time=market_open+delta_index
+        return str(current_time)
+
     # run the simulation once
     def run(self):
         current=self.prices[0]
         last_price=self.prices[0]
         self.buy(current)
 
+        trade_history=[]
         for i,price in enumerate(self.prices):
             last_price=price
+            traded=False
+            current_trade={}
             if self.has_stock:
                 if(price/current-1>=self.params['min_growth']):
                     self.sell(price)
+                    traded=True
+                    current_trade['type']="sell"
+                    current_trade['timestamp']=self.get_timestamp(i)+f' {self.file}'
+                    current_trade['price']=price
                     current=price
             else:
                 if(current/price-1<=self.params['min_decay']):
@@ -75,16 +93,23 @@ class Simulation():
                         custom_print("Went bankrupt...exiting simulation\n",0)
                         break
                     current=price
+                    traded=True
+                    current_trade['type']="buy"
+                    current_trade['timestamp']=self.get_timestamp(i)+f' {self.file}'
+                    current_trade['price']=price
+            if traded:
+                trade_history.append(current_trade)
 
         if(self.has_stock):
             custom_print("\nMarket is closing...selling the remaining shares\n",0)
             self.sell(last_price)
+            trade_history.append({"type":"sell","timestamp":self.get_timestamp(len(self.prices)-1)+f' {self.file}',"price":last_price})
 
         return_yield=(self.balance/self.params['starting_value']-1)*100
         custom_print(f"Simulation complete. Balance is {self.balance}. Return is {return_yield}%, or ${self.balance-self.params['starting_value']}",0)
         custom_print(f"Total transaction fees: ${self.transaction_fees}. # Round trip trades: {int(self.transaction_fees/self.transaction_fee/2)}",0)
 
-        return return_yield
+        return {"return_yield":return_yield,"trade_history":trade_history}
 
 # tunes a black box given parameters and the output to optimize.
 class Tuner():
@@ -127,8 +152,10 @@ class Tuner():
         results=[]
         for file in self.files:
             simulation=Simulation(file,instance_params)
-            return_yield=simulation.run()
-            results.append({"params":instance_params,"output":return_yield})
+            simulation_results=simulation.run()
+            return_yield=simulation_results['return_yield']
+            trade_history=simulation_results['trade_history']
+            results.append({"params":instance_params,"output":return_yield,"trade_history":trade_history})
         return results
 
     # run simulations where eatch parameter set is tested against all the days
@@ -139,10 +166,8 @@ class Tuner():
 
         passing_scenarios=[]
         num_scenarios=len(scenarios)
-        print_step=int(num_scenarios/100)
         for i,scenario in enumerate(scenarios):
-            if(i%print_step==0):
-                custom_print(f"Running scenario {i} of {len(scenarios)}",1)
+            custom_print(f"Running scenario {i} of {len(scenarios)}",1)
             result=self.run_dataset(scenario)
             result_valid=True
 
@@ -156,24 +181,29 @@ class Tuner():
 
 
 params={
-    "min_growth":{"start":0.01,"end":0.05,"step":0.01},
-    "min_decay":{"start":-0.05,"end":-0.01,"step":0.01},
-    "starting_value":{"start":1000,"end":5000,"step":500},
-    "num_shares":{"start":30,"end":60,"step":5}
+    "min_growth":{"start":0.01,"end":0.06,"step":0.01},
+    "min_decay":{"start":-0.06,"end":-0.01,"step":0.01},
+    "starting_value":{"start":500,"end":5000,"step":500},
+    "num_shares":{"start":10,"end":100,"step":10}
 }
 
 tuner=Tuner(params)
 result=tuner.run_tuner()
 
 scenarios=[]
+
 for scenario in result:
+    trade_histories=[]
+    yields=[]
     average_yield=0
     i=0
     for day in scenario:
         i=i+1
         average_yield=(average_yield+day["output"])/(i) 
+        trade_histories.append(day['trade_history'])
+        yields.append(day['output'])
     params=scenario[0]['params']
-    scenarios.append({"params":params,"average_yield":average_yield})
+    scenarios.append({"params":params,"average_yield":average_yield,"trade_history":trade_histories,"yield":yields})
 
 sorted_result= sorted(scenarios, key=lambda k: k['average_yield'],reverse=True)
 custom_print(sorted_result,0)
@@ -181,9 +211,9 @@ custom_print(f"\n{len(sorted_result)} passing parameters found",2)
 
 out_file_name="out_daytrade.html"
 with open(out_file_name,"w") as f:
-    sorted_result_json=json.dumps(sorted_result)
-    sorted_result_html=json2html.convert(json = sorted_result_json)
-    f.write(json.dumps(sorted_result_html))
+    best_result=json.dumps(sorted_result)
+    best_result_html=json2html.convert(json = best_result)
+    f.write(json.dumps(best_result_html))
 
 custom_print(f"Wrote output to file at {out_file_name}",2)
 
