cfc3fc9787bc4330baf11b33f971032984fc7775
Mahela-DESKTOP <mahela37@gmail.com>
2021-06-07 22:23:50 -0700

    formatted html file

--- daytrade.py
+++ daytrade.py
@@ -4,17 +4,17 @@ from custom_print import custom_print
 from tuner import Tuner
 
 
-params={
-    "min_growth":{"start":0.02,"end":0.04,"step":0.01},
+params_simulation={
+    "min_growth":{"start":0.04,"end":0.04,"step":0.01},
     "min_decay":{"start":-0.04,"end":-0.02,"step":0.01},
-    "starting_value":{"start":4000,"end":6000,"step":1000},
+    "starting_value":{"start":6000,"end":6000,"step":1000},
     "num_shares":{"start":50,"end":100,"step":5},
     "stop_loss":{"start":-0.4,"end":-0.03,"step":0.02},
     "min_drop_opening_purchase":{"start":0.05,"end":0.2,"step":0.03},
-    "min_minutes_before_first_buy":{"start":20,"end":40,"step":10}
+    "min_minutes_before_first_buy":{"start":40,"end":40,"step":10}
 }
 
-tuner=Tuner(params)
+tuner=Tuner(params_simulation)
 result=tuner.run_tuner()
 
 scenarios=[]
@@ -39,7 +39,7 @@ custom_print(f"\n{len(sorted_result)} passing parameters found",2)
 current_datetime=str(datetime.datetime.now()).split(".")[0].replace("-","_").replace(":","_").replace(" ","_")
 out_file_name=f"output_files/out_daytrade_{current_datetime}.html"
 with open(out_file_name,"w") as f:
-    out_html=json2html.convert(json = json.dumps({"params":params,"summary":sorted_result}))
+    out_html=json2html.convert(json = json.dumps({"params":params_simulation,"summary":sorted_result[0:10]}))
     f.write(json.dumps(out_html))
 
 custom_print(f"Wrote output to file at {out_file_name}",2)
